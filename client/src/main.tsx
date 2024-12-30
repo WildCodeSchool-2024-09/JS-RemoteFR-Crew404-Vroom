ccfd5aba7d73d02c6288f36a4b392373198f5dc2
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-12-30 11:48:28 +0100

    fix main.tsx. please, don't remove import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import About from "./pages/About/About";
