ecf5a5dbb551cea0f915ec2dbd5ca2b6164a9922
rudy flexcode <rudyrobert2018@gmail.com>
2025-01-07 18:52:31 +0400

    create Intro.tsx

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,12 +1,12 @@
 import Caroussel from "../../components/caroussel/Caroussel";
-// import Intro from "../../components/intro/intro";
+import Intro from "../../components/intro/Intro";
 
 function Home() {
   return (
     <>
       <header />
       <Caroussel />
-      {/* <Intro /> */}
+      <Intro />
 
       <main />
       <footer />
