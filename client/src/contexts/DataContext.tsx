94718c197b55d7ff1393aad9c1c5bad32d01f0c8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-17 13:49:37 +0100

    Déclaration des valeurs mise a disposition dans le context

--- client/src/contexts/DataContext.tsx
+++ client/src/contexts/DataContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext } from "react";
+import { useState } from "react";
 
 /* Import des différents composants ou img */
 
@@ -6,3 +7,6 @@ import { createContext, useContext } from "react";
 
 /* Création du context */
 const DataContext = createContext(null);
+
+/* Déclaration des valeurs mise a disposition dans le context */
+const [data, setData] = useState([]);
