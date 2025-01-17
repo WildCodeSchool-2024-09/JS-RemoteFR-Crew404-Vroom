95a05f42d2bcec907ec30cd8cef64f2baee2ac74
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-17 13:47:02 +0100

    Création du context

--- client/src/contexts/DataContext.tsx
+++ client/src/contexts/DataContext.tsx
@@ -3,3 +3,6 @@ import { createContext, useContext } from "react";
 /* Import des différents composants ou img */
 
 /* Déclarations des types */
+
+/* Création du context */
+const DataContext = createContext(null);
