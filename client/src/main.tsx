735aeddf4564a741593d4db30e585714602c4126
Charles Catto <charlescatto@outlook.fr>
2025-01-22 15:57:30 +0100

    Finalizing css on DashBoard.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/dashbord",
+        path: "/dashboard",
         element: <Dashboard />,
       },
       {
