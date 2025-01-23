8cae0b1d9814eb4a55cb394c74f6846e81bf2601
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-23 12:31:02 +0100

    Dégagement entier du contenu de la page home car il y a de grave problème d'affichage a tout les niveaux. Je vais pouvoir traiter problème par problème

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,9 @@
 import LogoVroom from "../../assets/Logos/LogoVroom.png";
-import Footer from "../../components/Footer/Footer";
+/*import Footer from "../../components/Footer/Footer";*/
 import NavRoot from "../../components/NavRoot/NavRoot";
 /*import Button from "../../components/button/Button";*/
 /*import Caroussel from "../../components/caroussel/Caroussel";*/
-import Intro from "../../components/intro/Intro";
+/*import Intro from "../../components/intro/Intro";*/
 import Styles from "./Home.module.css";
 
 function Home() {
@@ -12,11 +12,11 @@ function Home() {
       <NavRoot />
       <img className={Styles.logo} src={LogoVroom} alt="Logo" />
       <h1 className={Styles.welcome}>BIENVENUE SUR RoadAddict</h1>
-      {/*<Caroussel />
-      <Button />*/}
-      <Intro />
       <main />
-      <Footer />
+      {/*<Caroussel />
+      <Button />
+      <Intro />
+      <Footer />*/}
     </>
   );
 }
