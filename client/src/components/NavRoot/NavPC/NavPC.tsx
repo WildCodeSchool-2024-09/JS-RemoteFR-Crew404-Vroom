15d3ef5c8c126d9221791ccf699a298fd7500be8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-28 11:25:56 +0100

    Premier model de NavPC

--- client/src/components/NavRoot/NavPC/NavPC.tsx
+++ client/src/components/NavRoot/NavPC/NavPC.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Logo from "../../../assets/Logos/Logo-model-3.svg";
 import MenuNavRoot from "../ComponentsNavRoot/MenuNavRoot";
 import style from "../ComponentsNavRoot/MenuNavRoot.module.css";
 import css from "./NavPC.module.css";
@@ -11,29 +12,33 @@ function NavPC(Props: NavPCProps) {
   const { namePage } = Props;
   return (
     <nav className={css.NavPC}>
-      <div className={css.NavTopPC}>
-        {/*Le style de ce composant est directement géré par le module CSS :
-                 MenuNavRoot.module.css*/}
-        <MenuNavRoot
-          moduleMenuUl={style.MenuUlPC}
-          moduleMenuLi={style.MenuLiPC}
-          moduleMenuLink={style.MenuLinkPC}
-          moduleMenuLiNone={style.MenuLiNonePC}
-          moduleMenuNavRoot={style.MenuNavRootPC}
-        />
+      {/*Le style de ce composant est directement géré par le module CSS :
+        MenuNavRoot.module.css*/}
+      <MenuNavRoot
+        moduleMenuUl={style.MenuUlPC}
+        moduleMenuLi={style.MenuLiPC}
+        moduleMenuLink={style.MenuLinkPC}
+        moduleMenuLiNone={style.MenuLiNonePC}
+        moduleMenuNavRoot={style.MenuNavRootPC}
+      />
 
+      <div className={css.NamePageNavPC}>
+        <Link to="/">
+          <h1>{namePage}</h1>
+        </Link>
+      </div>
+
+      <div className={css.ContainerLogLogoNavPC}>
         <li className={style.MenuLiPC}>
           <Link to="/conne" className={style.MenuLinkPC}>
             Connexion / Inscription
           </Link>
         </li>
-      </div>
 
-      <section className={css.NavBottomPC}>
-        <Link to="/">
-          <h1>{namePage}</h1>
-        </Link>
-      </section>
+        <div className={css.LogoNavPC}>
+          <img src={Logo} alt="Logo RoadAddict" />
+        </div>
+      </div>
     </nav>
   );
 }
