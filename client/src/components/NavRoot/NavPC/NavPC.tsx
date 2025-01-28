db5d6a55bf8c7955acfbb87ecc1cada9ff670862
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-28 15:48:40 +0100

    Ajustement des tailles des containers de NavPC

--- client/src/components/NavRoot/NavPC/NavPC.tsx
+++ client/src/components/NavRoot/NavPC/NavPC.tsx
@@ -9,59 +9,62 @@ interface NavPCProps {
 function NavPC(Props: NavPCProps) {
   const { namePage } = Props;
   return (
-    <nav className={css.NavPC}>
-      <div className={css.ContainerLogNavPC}>
-        <div className={css.LogoNavPC}>
-          <img src={Logo} alt="Logo RoadAddict" />
+    <>
+      <div className={css.CaleNavPC} />
+      <nav className={css.NavPC}>
+        <div className={css.ContainerLogNavPC}>
+          <div className={css.LogoNavPC}>
+            <img src={Logo} alt="Logo RoadAddict" />
+          </div>
+
+          <ul className={css.MenuUlPC}>
+            <li className={css.MenuLiPC}>
+              <Link to="/maps" className={css.MenuLinkPC}>
+                Maps
+              </Link>
+            </li>
+
+            <li className={css.SousNavLiPC}>
+              <span className={css.SousNavSpanPC}>Nous ↓</span>
+
+              <ul className={css.SousMenuUlPC}>
+                <li className={css.SousMenuLiPC}>
+                  <Link to="" className={css.SousMenuLinkPC}>
+                    Contact
+                  </Link>
+                </li>
+
+                <li className={css.SousMenuLiPC}>
+                  <Link to="" className={css.SousMenuLinkPC}>
+                    A Propos
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
 
-        <ul className={css.MenuUlPC}>
+        <div className={css.NamePageNavPC}>
+          <Link to="/">
+            <h1>{namePage}</h1>
+          </Link>
+        </div>
+
+        <div className={css.ContainerLogNavPC}>
           <li className={css.MenuLiPC}>
-            <Link to="/maps" className={css.MenuLinkPC}>
-              Maps
+            <Link to="/dashbord" className={css.MenuLinkPC}>
+              Tableau de bord
             </Link>
           </li>
 
-          <li className={css.SousNavLiPC}>
-            <span className={css.SousNavSpanPC}>Nous ↓</span>
-
-            <ul className={css.SousMenuUlPC}>
-              <li className={css.SousMenuLiPC}>
-                <Link to="" className={css.SousMenuLinkPC}>
-                  Contact
-                </Link>
-              </li>
-
-              <li className={css.SousMenuLiPC}>
-                <Link to="" className={css.SousMenuLinkPC}>
-                  A Propos
-                </Link>
-              </li>
-            </ul>
+          <li className={css.MenuLiPC}>
+            <Link to="/account" className={css.MenuLinkPC}>
+              Compte
+            </Link>
           </li>
-        </ul>
-      </div>
-
-      <div className={css.NamePageNavPC}>
-        <Link to="/">
-          <h1>{namePage}</h1>
-        </Link>
-      </div>
-
-      <div className={css.ContainerLogNavPC}>
-        <li className={css.MenuLiPC}>
-          <Link to="/dashbord" className={css.MenuLinkPC}>
-            Tableau de bord
-          </Link>
-        </li>
-
-        <li className={css.MenuLiPC}>
-          <Link to="/account" className={css.MenuLinkPC}>
-            Compte
-          </Link>
-        </li>
-      </div>
-    </nav>
+        </div>
+      </nav>
+    </>
   );
 }
 
--- client/src/components/NavRoot/NavRoot.tsx
+++ client/src/components/NavRoot/NavRoot.tsx
@@ -1,5 +1,6 @@
 import NavMobile from "./NavMobile/NavMobile";
 import NavPC from "./NavPC/NavPC";
+import "./NavRoot.css";
 
 interface NavRootProps {
   namePage: string;
