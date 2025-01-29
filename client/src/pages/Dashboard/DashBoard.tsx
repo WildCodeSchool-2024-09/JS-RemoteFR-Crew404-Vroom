c84855078ffe7c95c22fb4cc0b939e3b0f277b07
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-29 12:40:33 +0100

    Nettoyage de Dashboard

--- client/src/pages/Dashboard/DashBoard.tsx
+++ client/src/pages/Dashboard/DashBoard.tsx
@@ -1,7 +1,5 @@
-import Header from "../../components/Header/Header";
 import NavRoot from "../../components/NavRoot/NavRoot";
 import Advert from "./Advert/Advert";
-import styles from "./Dashboard.module.css";
 import Event from "./Event/Event";
 import Points from "./Points/Points";
 import Vehicule from "./Vehicule/Vehicule";
