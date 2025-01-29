5d0726d5a7e2144327f34dc9e449f5c5c203ba93
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-29 12:53:21 +0100

    Intégration de NavRoot dans Contact

--- client/src/pages/Contact/Contact.tsx
+++ client/src/pages/Contact/Contact.tsx
@@ -4,12 +4,13 @@ import envelopeIcon from "../../assets/images/social/envelope.png";
 import facebookIcon from "../../assets/images/social/facebook.png";
 import instagramIcon from "../../assets/images/social/instagram.png";
 import Header from "../../components/Header/Header";
+import NavRoot from "../../components/NavRoot/NavRoot";
 import styles from "./Contact.module.css";
 
 function Contact() {
   return (
     <>
-      <Header title="CONTACT" />
+      <NavRoot namePage="CONTACT" />
       <div className={styles["contact-container"]}>
         <img
           src={contactHeroBanner}
