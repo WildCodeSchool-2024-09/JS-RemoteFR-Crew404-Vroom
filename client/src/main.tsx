5882b78c5eef6a4a2dfb4ac66c273fd2640c901b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-01-29 13:24:05 +0100

    Déplacement de HelmetProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import { HelmetProvider } from "react-helmet-async";
 /**
  * Contexts
  */
