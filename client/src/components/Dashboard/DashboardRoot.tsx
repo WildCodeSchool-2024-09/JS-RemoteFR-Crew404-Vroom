2b423d6dfdbc4d6344babd38581ac0b9d4b49516
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-02-05 15:48:23 +0100

    Délocalisation de la logique de Dashbord dans DashbordRoot. Le composant est prêt a récupérer reactHelmet

--- client/src/components/Dashboard/DashboardRoot.tsx
+++ client/src/components/Dashboard/DashboardRoot.tsx
@@ -1,7 +1,20 @@
 import "./DashboardRoot.css";
 
+/* Impot des composants */
+import Advert from "../../pages/Dashboard/Advert/Advert";
+import Event from "../../pages/Dashboard/Event/Event";
+import Points from "../../pages/Dashboard/Points/Points";
+import Vehicule from "../../pages/Dashboard/Vehicule/Vehicule";
+
 function DashboardRoot() {
-  return <></>;
+  return (
+    <>
+      <Advert />
+      <Event />
+      <Points />
+      <Vehicule />
+    </>
+  );
 }
 
 export default DashboardRoot;
--- client/src/pages/Dashboard/DashBoard.tsx
+++ client/src/pages/Dashboard/DashBoard.tsx
@@ -1,17 +1,16 @@
+import DashboardRoot from "../../components/Dashboard/DashboardRoot";
 import NavRoot from "../../components/NavRoot/NavRoot";
-import Advert from "./Advert/Advert";
-import Event from "./Event/Event";
-import Points from "./Points/Points";
-import Vehicule from "./Vehicule/Vehicule";
 
 function Dashboard() {
   return (
     <>
       <NavRoot namePage="Tableau de bord" />
-      <Points />
-      <Event />
-      <Vehicule />
-      <Advert />
+
+      <main>
+        <DashboardRoot />
+      </main>
+
+      <footer />
     </>
   );
 }
