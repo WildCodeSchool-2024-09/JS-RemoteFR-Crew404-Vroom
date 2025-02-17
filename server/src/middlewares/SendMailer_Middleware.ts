938858468d475caf8a5d0bae6140378110d866c8
Ryan DECIAN <ryan.decian.propgmail.com>
2025-02-17 15:58:55 +0100

    Ajout des middlewares de node mailer

---
+++ server/src/middlewares/SendMailer_Middleware.ts
@@ -0,0 +1,34 @@
+import type { NextFunction, Request, Response } from "express";
+import mailer from "../services/mailer";
+import "dotenv/config";
+
+const transporter = mailer;
+
+async function SendMailer_Middleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const info = await transporter.sendMail({
+      from: `"RoadAddict" <${process.env.EMAIL_USER}>`, // email de l'expéditeur
+      to: req.body.to, // liste email des destinataires
+      subject: req.body.subject, // objet du mail
+      text: req.body.text, // Contenu du mail
+      // html: "", // html body
+    });
+    next();
+  } catch (error) {
+    res.status(500).json({ error: "Erreur interne serveur." });
+    console.error({
+      identity: "SendMailer_Middleware.ts",
+      type: "middleware",
+      chemin: "/server/src/middleware/SendMailer_Middleware.ts",
+      "❌ Nature de l'erreur": "Mail non envoyé !",
+      details: error,
+    });
+    return;
+  }
+}
+
+export default SendMailer_Middleware;
