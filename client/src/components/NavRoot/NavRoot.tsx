6058b6ce4ab6e2d4eaef484240421537f6565741
Antoine Chauvot <antoine_chauvot@hotmail.com>
2025-02-19 08:30:16 +0100

    Update NavRoot.tsx
    bye code commenté

--- client/src/components/NavRoot/NavRoot.tsx
+++ client/src/components/NavRoot/NavRoot.tsx
@@ -1,23 +1,3 @@
-// import NavMobile from "./NavMobile/NavMobile";
-// import NavPC from "./NavPC/NavPC";
-// import "./NavRoot.css";
-
-// interface NavRootProps {
-//   namePage: string;
-// }
-
-// function NavRoot(Props: NavRootProps) {
-//   const { namePage } = Props;
-//   return (
-//     <header className="headerRoot">
-//       <NavMobile namePage={namePage} />
-//       <NavPC namePage={namePage} />
-//     </header>
-//   );
-// }
-
-// export default NavRoot;
-
 import NavMobile from "./NavMobile/NavMobile";
 import NavPC from "./NavPC/NavPC";
 import "./NavRoot.css";
